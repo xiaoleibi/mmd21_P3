863a73c1876948624eab92b907bfa0214b7e7962
xiaoleibi <bixiaolei@hotmail.com>
2022-11-18 11:07:50 +0100

    x

--- slet_i_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!empty($_GET['id'])){
-$id= $_GET['id'];
-include_once 'connection.php';
-$fordring = "DELETE FROM opskrifter WHERE id=$id;";
-$svar = $forbindelse->query($fordring);
-header('location: indexkage.php');
-}
-?>
